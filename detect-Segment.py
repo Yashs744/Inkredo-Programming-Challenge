3f80d34e77617c89ccce854b0c64537a7a795a69
Yash Sharma <yash3498@gmail.com>
2018-11-03 17:31:34 +0530

    removing noise

--- detect-Segment.py
+++ detect-Segment.py
@@ -30,18 +30,33 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Bilateral Filter to Smoothout the Image
 image = cv2.bilateralFilter(image, 11, 50, 50)
 
+# Adaptive Threshold to remove noise from the image 
+# It would tranform the image into an binary image
+# Documentation at
+#   https://docs.opencv.org/3.4.0/d7/d1b/group__imgproc__misc.html#ga72b913f352e4a1b1b397736707afcde3
+image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 4)
+
+# Median filter would help in filtering or cleaning small details from the image
+image = cv2.medianBlur(image, 11)
+
 # Detecting Edges using Canny Edge Detector
-edges = cv2.Canny(image, 75, 200)
+edges = cv2.Canny(image, 200, 250)
 
 # Find Contours in the Edge Image
 _, contour, __ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-contour = sorted(contour, key = cv2.contourArea, reverse = False)[:5]
+contour = sorted(contour, key = cv2.contourArea, reverse = True)[:10]
 
+# for each contour
 for cnts in contour:
-    i = cv2.arcLength(cnts, True)
-    approx_value = cv2.approxPolyDP(cnts, 0.02 * i, True)
 
-    if len(approx_value) == 2 or len(approx_value) == 4:
+    # Find the Perimeter
+    i = cv2.arcLength(cnts, True)
+    # Approximate the Curve
+    approx_value = cv2.approxPolyDP(cnts, 0.03 * i, True)
+
+    if len(approx_value) == 4 or len(approx_value) == 2:
+
+        # Draw the Contour and Display the Image
         cv2.drawContours(image, [approx_value], -1, (0, 255, 0), 2)
 
         cv2.imshow("Contour Image", image)
