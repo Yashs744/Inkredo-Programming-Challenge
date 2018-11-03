d98cd78af91ad83440a4c98f621db028195e6926
Yash Sharma <yash3498@gmail.com>
2018-11-03 18:14:29 +0530

    Transform the Image

--- detect-Segment.py
+++ detect-Segment.py
@@ -7,12 +7,17 @@ import numpy as np
 image_path = "../DataSet/DIQ_Part1/set12/2012-04-19_18-27-07_716.jpg"
 
 # Steps to Detect Document in the Image
-#     1. Load the Image and resize it.
-#     2. Convert the Image to GrayScale.
-#     3. Use Bilateral Filter to Smooth out the Image.
-#     4. Detect Edges using Canny Edge Detector.
-#     5. Find Contours in the Edge Detected Image.
-#     6. Pick Points which Cover the Document.
+#    1. Load the Image and resize it.
+#    2. Convert the Image to GrayScale.
+#    3. Image Process
+#       3.1. Bilaternal Filter to reduce Noise and smooth out image.
+#       3.2. Adaptive Threshold to Convert the Image to an Binary Image
+#       3.3. Median Blur to filter out small pixel details
+#       3.4. Black Border around Image to handle Documents that maybe touching the Border of Image
+#    4. Detect Edges using Canny Edge Detector.
+#    5. Find Contours in the Edge Detected Image.
+#    6. Pick Points which Cover the Document.
+#    7. Transform the Image
 
 # 1. Read the Image using opencv imread() function
 image = cv2.imread(image_path)
@@ -62,9 +67,48 @@ for cnts in contour:
 
     # Assuming Document has 4 Corners
     if len(approx_value) == 4:
+        points = approx_value
+        break;
 
-        # Draw the Contour and Display the Image
-        cv2.drawContours(image, [approx_value], -1, (0, 255, 0), 2)
+# Find Corner Points i.e Top-left, Top-right, Bottom-left, Bottom-right
+pts = points
+points = points.reshape(4, 2)
 
-        cv2.imshow("Contour Image", image)
-        cv2.waitKey(0)
+diff = np.diff(points, axis=1)
+summ = points.sum(axis=1)
+
+# Top
+# Top-left have the Smalles Sum and Bottom-right have the maximum summ
+
+# Bottom
+# Top-right have the minimum difference and Bottom-left have the maximum difference.
+rectange = np.array([points[np.argmin(summ)], points[np.argmax(diff)], points[np.argmax(summ)], points[np.argmin(diff)]])
+
+# Offset Contour, by 4px border
+offset = (-4, -4)       
+rectange += offset	    	       
+rectange[rectange < 0] = 0
+
+# Rescale the Image to Original
+rescaled = pts.dot(original_image.shape[0] / height)
+
+# Calculate the Height and Width of New Image
+height = max(np.linalg.norm(rescaled[0] - rescaled[1]), np.linalg.norm(rescaled[2] - rescaled[3]))
+width = max(np.linalg.norm(rescaled[1] - rescaled[2]), np.linalg.norm(rescaled[3] - rescaled[0]))
+
+# Target Points to View the New Image from correct angel.
+target = np.array([[0, 0],[0, height],[width, height],[width, 0]], np.float32)
+
+# Convert rescaled image to float32
+rescaled = rescaled.astype(np.float32)
+
+# Get the Perspective
+M = cv2.getPerspectiveTransform(rescaled, target)
+
+# Wrap the Image to get a new Image
+output = cv2.warpPerspective(original_image, M, (int(width), int(height)))
+
+# Display the Output Image
+cv2.imshow("Original Image", original_image)
+cv2.imshow("Output", output)
+cv2.waitKey(0)
